4b6206a84acc8b5f1db63aca77bd23d353fe6cdd
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-24 20:25:36 +0600

    overview video added using react-player

--- src/components/Home/Overview.jsx
+++ src/components/Home/Overview.jsx
@@ -1,8 +1,11 @@
+// Only loads the YouTube player
+import ReactPlayer from 'react-player/youtube'
 
 const Overview = () => {
     return (
         <div>
             overview
+            <ReactPlayer url='https://www.youtube.com/watch?v=LXb3EKWsInQ' />
         </div>
     );
 };
