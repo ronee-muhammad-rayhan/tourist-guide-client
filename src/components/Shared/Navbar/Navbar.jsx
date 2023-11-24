66540511d62337fe34b2322605ab07d1b8044e14
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-24 15:45:25 +0600

    nav routes added

--- src/components/Shared/Navbar/Navbar.jsx
+++ src/components/Shared/Navbar/Navbar.jsx
@@ -1,6 +1,5 @@
 import './Navbar.css'
 import { Avatar, Dropdown, Navbar } from "flowbite-react";
-import { Outlet } from "react-router-dom";
 import profileImage from "./../../../assets/images/profile-picture-5.jpg";
 
 export default function NavbarFlowbite() {
@@ -8,7 +7,7 @@ export default function NavbarFlowbite() {
 
     return (
         <nav>
-            <Navbar fluid={true} rounded={true}>
+            <Navbar fluid={true} rounded={true} >
                 <Navbar.Brand href="https://flowbite.com/">
                     <img
                         src="https://flowbite.com/docs/images/logo.svg"
@@ -16,7 +15,7 @@ export default function NavbarFlowbite() {
                         alt="Flowbite Logo"
                     />
                     <span className="self-center whitespace-nowrap text-xl font-semibold dark:text-white">
-                        Flowbite
+                        Tourist Guide
                     </span>
                 </Navbar.Brand>
                 <div className="flex md:order-2">
@@ -32,29 +31,31 @@ export default function NavbarFlowbite() {
                         }
                     >
                         <Dropdown.Header>
-                            <span className="block text-sm">Bonnie Green</span>
+                            <span className="block text-sm">RoneeMRayhan</span>
                             <span className="block truncate text-sm font-medium">
-                                name@flowbite.com
+                                roneemrayhan@rayhanworld.com
                             </span>
                         </Dropdown.Header>
                         <Dropdown.Item>Dashboard</Dropdown.Item>
                         <Dropdown.Item>Settings</Dropdown.Item>
-                        <Dropdown.Item>Earnings</Dropdown.Item>
+                        <Dropdown.Item>Offer Announcements</Dropdown.Item>
                         <Dropdown.Divider />
-                        <Dropdown.Item>Sign out</Dropdown.Item>
+                        <Dropdown.Item>Logout</Dropdown.Item>
                     </Dropdown>
                     <Navbar.Toggle />
                 </div>
                 <Navbar.Collapse>
-                    <Navbar.Link href="/navbars" active={true}>
+                    <Navbar.Link href="/" active={true}>
                         Home
                     </Navbar.Link>
-                    <Navbar.Link href="/expenses">Patients</Navbar.Link>
-                    <Navbar.Link href="/navbars">Services</Navbar.Link>
-                    <Navbar.Link href="/navbars">Pricing</Navbar.Link>
-                    <Navbar.Link href="/navbars">Contact</Navbar.Link>
+                    <Navbar.Link href="/community">Community</Navbar.Link>
+                    <Navbar.Link href="/blogs">Blogs</Navbar.Link>
+                    <Navbar.Link href="/about">About Us</Navbar.Link>
+                    <Navbar.Link href="/contact">Contact Us</Navbar.Link>
+                    <Navbar.Link href="/login">Login</Navbar.Link>
+                    <Navbar.Link href="/register">Register</Navbar.Link>
                 </Navbar.Collapse>
             </Navbar>
-        </nav>
+        </nav >
     );
 }
