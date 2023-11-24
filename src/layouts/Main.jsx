bd18810a357c4b1ece980c54f62ab5ab11ad0483
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-24 16:16:01 +0600

    footer added

--- src/layouts/Main.jsx
+++ src/layouts/Main.jsx
@@ -1,12 +1,14 @@
 import { Outlet } from 'react-router-dom'
 import NavbarFlowbite from '../components/Shared/Navbar/Navbar'
+import Footer from '../components/Shared/Footer/Footer'
 const Main = () => {
     return (
         <div>
             <NavbarFlowbite></NavbarFlowbite>
-            <div>
+            <div className='pt-24 min-h-[calc(100vh-68px)]'>
                 <Outlet></Outlet>
             </div>
+            <Footer></Footer>
         </div>
     )
 }
