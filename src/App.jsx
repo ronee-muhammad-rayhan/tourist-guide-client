24c6f1b2ce51ff3f4c52d219262b1cff9fe3f286
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-24 12:02:28 +0600

    flowbite-react implemented

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,17 @@
+import { Button } from 'flowbite-react'
 import './App.css'
 
 function App() {
 
   return (
-    <h1 className="text-3xl font-bold underline">
-      Hello world!
-    </h1>
+    <>
+      <h1 className="text-3xl font-bold underline">
+        Hello world!
+      </h1>
+      <div>
+        <Button>Click me</Button>
+      </div>
+    </>
   )
 }
 
