53a5cbdfb6cc22c542da7ecab86a7195c33284bb
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-24 12:59:31 +0600

    flowbite-react navbar added

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,12 @@
 import { Button } from 'flowbite-react'
 import './App.css'
+import NavbarFlowbite from './components/Shared/Navbar/Navbar'
 
 function App() {
 
   return (
     <>
+      <NavbarFlowbite></NavbarFlowbite>
       <h1 className="text-3xl font-bold underline">
         Hello world!
       </h1>
