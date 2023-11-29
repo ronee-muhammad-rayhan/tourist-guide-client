5a859baaf5c7715d8235a30ba7583bbaf3bcef0e
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-30 02:58:27 +0600

    bug fixed

--- src/pages/MyBookings/MyBookings.jsx
+++ src/pages/MyBookings/MyBookings.jsx
@@ -14,6 +14,8 @@ const MyBookings = () => {
     const { user } = useAuth();
     // const [bookings, setBookings] = useState([]);
     const [status, setStatus] = useState('InReview');
+    // const [apply, setApply] = useState(false);
+    let apply = false
     // const [bookings] = useBookings();
     const statusArray = ['InReview', 'Rejected', 'Accepted']
     // const { width, height } = useWindowSize()
@@ -30,6 +32,9 @@ const MyBookings = () => {
 
     if (bookings.length > 3) {
         console.log('confetti');
+        apply = true;
+        // setApply(true);
+        // return
     }
 
     /* useEffect(() => {
@@ -168,99 +173,102 @@ const MyBookings = () => {
     const handlePay = () => { }
 
     return (
-        <form>
-            <h3 className="text-4xl">My Total Bookings: {bookings.length}</h3>
-            <div>
-                <div className="container p-2 mx-auto sm:p-4 dark:text-gray-100 overflow-x-auto">
-                    <h2 className="mb-4 text-2xl font-semibold leadi">Bookings</h2>
-                    <div className="overflow-x-auto">
-                        <table className="min-w-full text-xs overflow-x-auto">
-                            <colgroup className="overflow-x-auto">
-                                <col />
-                                <col />
-                                <col />
-                                <col />
-                                <col />
-                                <col className="w-24" />
-                            </colgroup>
-                            <thead className="dark:bg-gray-700 overflow-x-auto">
-                                <tr className="text-left">
-                                    <th className="p-3">#</th>
-                                    <th className="p-3">Name of the Package</th>
-                                    <th className="p-3">Tour Guide Name</th>
-                                    <th className="p-3">Tour Date</th>
-                                    <th className="p-3 text-right">Tour Price ($)</th>
-                                    <th className="p-3">Status</th>
-                                    <th className="p-3">Actions</th>
-                                </tr>
-                            </thead>
-                            <tbody className="overflow-x-auto">
-                                {
-                                    bookings.map((booking, i) => {
-                                        return (
-                                            <tr id={booking?._id} key={i} className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
-                                                <td className="p-3">
-                                                    <p>{i + 1}</p>
-                                                </td>
-                                                <td className="p-3">
-                                                    <p>{booking?.tourPackageTitle}</p>
-                                                </td>
-                                                <td className="p-3">
-                                                    {/* <p>14 Jan 2022</p> */}
-                                                    <p className="dark:text-gray-400">{booking?.guide}</p>
-                                                </td>
-                                                <td className="p-3">
-                                                    <p>{booking?.date}</p>
-                                                    {/* <p className="dark:text-gray-400">Tuesday</p> */}
-                                                </td>
-                                                <td className="p-3 text-right">
-                                                    <p>{`$${booking?.price}`}</p>
-                                                </td>
-                                                <td className="p-3 text-right">
-                                                    <div className="col-span-full sm:col-span-3">
-                                                        {/* <label htmlFor="guide" className="text-sm">Your Tour Guide</label> */}
-                                                        <select disabled={(booking?.status === 'Accepted')} defaultValue={booking?.status} id="status" onChange={handleStatus} className="w-32">
-                                                            {/* <option value="">Select a Guide</option> */}
-                                                            {statusArray?.map((el, index) => (
-                                                                <option className="w-20" key={index} value={el}>
-                                                                    {el}
-                                                                </option>
-                                                            ))}
-                                                        </select>
-                                                    </div>
-                                                </td>
-                                                {/* <td className="p-3 text-right">
+        <>
+            <form>
+                <h3 className="text-4xl">My Total Bookings: {bookings.length}</h3>
+                <div>
+                    <div className="container p-2 mx-auto sm:p-4 dark:text-gray-100 overflow-x-auto">
+                        <h2 className="mb-4 text-2xl font-semibold leadi">Bookings</h2>
+                        <div className="overflow-x-auto">
+                            <table className="min-w-full text-xs overflow-x-auto">
+                                <colgroup className="overflow-x-auto">
+                                    <col />
+                                    <col />
+                                    <col />
+                                    <col />
+                                    <col />
+                                    <col className="w-24" />
+                                </colgroup>
+                                <thead className="dark:bg-gray-700 overflow-x-auto">
+                                    <tr className="text-left">
+                                        <th className="p-3">#</th>
+                                        <th className="p-3">Name of the Package</th>
+                                        <th className="p-3">Tour Guide Name</th>
+                                        <th className="p-3">Tour Date</th>
+                                        <th className="p-3 text-right">Tour Price ($)</th>
+                                        <th className="p-3">Status</th>
+                                        <th className="p-3">Actions</th>
+                                    </tr>
+                                </thead>
+                                <tbody className="overflow-x-auto">
+                                    {
+                                        bookings.map((booking, i) => {
+                                            return (
+                                                <tr id={booking?._id} key={i} className="border-b border-opacity-20 dark:border-gray-700 dark:bg-gray-900">
+                                                    <td className="p-3">
+                                                        <p>{i + 1}</p>
+                                                    </td>
+                                                    <td className="p-3">
+                                                        <p>{booking?.tourPackageTitle}</p>
+                                                    </td>
+                                                    <td className="p-3">
+                                                        {/* <p>14 Jan 2022</p> */}
+                                                        <p className="dark:text-gray-400">{booking?.guide}</p>
+                                                    </td>
+                                                    <td className="p-3">
+                                                        <p>{booking?.date}</p>
+                                                        {/* <p className="dark:text-gray-400">Tuesday</p> */}
+                                                    </td>
+                                                    <td className="p-3 text-right">
+                                                        <p>{`$${booking?.price}`}</p>
+                                                    </td>
+                                                    <td className="p-3 text-right">
+                                                        <div className="col-span-full sm:col-span-3">
+                                                            {/* <label htmlFor="guide" className="text-sm">Your Tour Guide</label> */}
+                                                            <select disabled={(booking?.status === 'Accepted')} defaultValue={booking?.status} id="status" onChange={handleStatus} className="w-32">
+                                                                {/* <option value="">Select a Guide</option> */}
+                                                                {statusArray?.map((el, index) => (
+                                                                    <option className="w-20" key={index} value={el}>
+                                                                        {el}
+                                                                    </option>
+                                                                ))}
+                                                            </select>
+                                                        </div>
+                                                    </td>
+                                                    {/* <td className="p-3 text-right">
                                                     <span className="px-3 py-1 font-semibold rounded-md dark:bg-violet-400 dark:text-gray-900">
                                                         <span>In Review</span>
                                                     </span>
                                                 </td> */}
-                                                <td className="p-3 text-right">
+                                                    <td className="p-3 text-right">
 
-                                                    <div className="text-center space-x-2 space-y-1">
-                                                        {<button disabled={!(booking?.status === 'Accepted')} /* disabled={isActionApplied} */ onClick={() => handleApply(user)} className="btn btn-xs bg-yellow-500">
-                                                            <h3 className="text-white text-md">Apply</h3>
-                                                        </button>}
-                                                        {
-                                                            <input type="submit" disabled={(booking?.status === 'Accepted')} hidden={!(booking?.status === 'InReview')} value={`Cancel`} /* disabled={isActionApplied} */ onClick={handleCancel} className={`btn btn-xs bg-red-500 text-white text-md ${booking?.status === 'InReview' ? 'hidden' : 'inline-block'}`} />
-                                                        }
-                                                        {<button disabled={!(booking?.status === 'Accepted')} /* disabled={isActionApplied} */ onClick={() => handlePay(user)} className="btn btn-xs bg-lime-500">
-                                                            <h3 className="text-white text-md">Pay</h3>
-                                                        </button>}
-                                                        {/* <button disabled onClick={() => handleDeleteUser(user)} className="btn btn-ghost btn-lg">
+                                                        <div className="text-center space-x-2 space-y-1">
+                                                            {<button disabled={!apply} /* disabled={isActionApplied} */ onClick={() => handleApply(user)} className="btn btn-xs bg-yellow-500">
+                                                                <h3 className="text-white text-md">Apply</h3>
+                                                            </button>}
+                                                            {
+                                                                <input type="submit" disabled={(booking?.status === 'Accepted')} hidden={!(booking?.status === 'InReview')} value={`Cancel`} /* disabled={isActionApplied} */ onClick={handleCancel} className={`btn btn-xs bg-red-500 text-white text-md ${booking?.status === 'InReview' ? 'hidden' : 'inline-block'}`} />
+                                                            }
+                                                            {<button disabled={!(booking?.status === 'Accepted')} /* disabled={isActionApplied} */ onClick={() => handlePay(user)} className="btn btn-xs bg-lime-500">
+                                                                <h3 className="text-white text-md">Pay</h3>
+                                                            </button>}
+                                                            {/* <button disabled onClick={() => handleDeleteUser(user)} className="btn btn-ghost btn-lg">
                                         <FaTrashAlt className="text-red-600"></FaTrashAlt>
                                     </button> */}
-                                                    </div>
+                                                        </div>
 
-                                                </td>
-                                            </tr>
-                                        )
-                                    })
-                                }
-                            </tbody>
-                        </table>
+                                                    </td>
+                                                </tr>
+                                            )
+                                        })
+                                    }
+                                </tbody>
+                            </table>
+                        </div>
                     </div>
                 </div>
-            </div>
+
+            </form>
             <Confetti
                 tweenDuration={13000}
                 numberOfPieces={3000}
@@ -279,7 +287,7 @@ const MyBookings = () => {
                     ctx.closePath()
                 }}
             />
-        </form>
+        </>
     );
 };
 
