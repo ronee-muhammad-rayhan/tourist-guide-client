30d812fc1f42140e86c9d9673361a9be02556301
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-29 13:55:04 +0600

    booking form added

--- src/pages/PackageDetails/PackageDetails.jsx
+++ src/pages/PackageDetails/PackageDetails.jsx
@@ -1,9 +1,11 @@
 import { useLoaderData } from "react-router-dom";
 import useGuides from "../../hooks/useGuides";
+import useAuth from "../../hooks/useAuth";
 
 const PackageDetails = () => {
     const tour = useLoaderData();
     const [guides] = useGuides();
+    const { user } = useAuth();
 
     const handleSelectGuide = (e) => {
         const selectedGuide = e.target.value;
@@ -34,7 +36,7 @@ const PackageDetails = () => {
                     ))}
                 </ul>
 
-                <h3>Guides:</h3>
+                {/* <h3>Guides:</h3> */}
 
 
                 {/* <select onChange={handleSelectGuide}>
@@ -46,14 +48,14 @@ const PackageDetails = () => {
                     ))}
                 </select> */}
 
-                <select onChange={handleSelectGuide}>
+                {/* <select onChange={handleSelectGuide}>
                     <option value="">Select a Guide</option>
                     {guides?.map((guide, index) => (
                         <option key={index} value={guide?.email}>
                             {guide?.name}
                         </option>
                     ))}
-                </select>
+                </select> */}
 
                 {/* <h3>{guides.map((guide,i)=>{
 
@@ -65,8 +67,80 @@ const PackageDetails = () => {
                                 ))}
                             </ul> */}
 
-                <p>Price: {tour?.price}</p>
+                {/* <p>Price: {tour?.price}</p> */}
             </div>
+
+            {/* Booking form */}
+            <section className="p-6 dark:bg-gray-800 dark:text-gray-50">
+                <form noValidate="" action="" className="container flex flex-col mx-auto space-y-12">
+                    <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm dark:bg-gray-900">
+                        <div className="grid grid-cols-6 gap-4 col-span-full">
+                            <div className="col-span-full sm:col-span-3">
+                                <label htmlFor="touristname" className="text-sm">Your name</label>
+                                <input id="touristname" type="text" defaultValue={user?.displayName} placeholder="Put your name here" className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" />
+                            </div>
+                            <div className="col-span-full sm:col-span-3">
+                                <label htmlFor="email" className="text-sm">Email</label>
+                                <input id="email" type="email" defaultValue={user?.email} readOnly disabled required placeholder="Email" className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" />
+                            </div>
+                            <div className="col-span-full">
+                                <label htmlFor="address" className="text-sm">Address</label>
+                                <input id="address" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" spellCheck="false" data-ms-editor="true" />
+                            </div>
+                            <div className="col-span-full sm:col-span-2">
+                                <label htmlFor="city" className="text-sm">City</label>
+                                <input id="city" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" spellCheck="false" data-ms-editor="true" />
+                            </div>
+                            <div className="col-span-full sm:col-span-2">
+                                <label htmlFor="state" className="text-sm">State / Province</label>
+                                <input id="state" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" spellCheck="false" data-ms-editor="true" />
+                            </div>
+                            <div className="col-span-full sm:col-span-2">
+                                <label htmlFor="zip" className="text-sm">ZIP / Postal</label>
+                                <input id="zip" type="text" placeholder="" className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" spellCheck="false" data-ms-editor="true" />
+                            </div>
+                        </div>
+                    </fieldset>
+                    <fieldset className="grid grid-cols-4 gap-6 p-6 rounded-md shadow-sm dark:bg-gray-900">
+                        <div className="grid grid-cols-6 gap-4 col-span-full lg:col-span-3">
+                            <div className="col-span-full sm:col-span-3">
+                                <label htmlFor="guide" className="text-sm">Your Tour Guide</label>
+                                <select id="guide" onChange={handleSelectGuide} className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900">
+                                    <option value="">Select a Guide</option>
+                                    {guides?.map((guide, index) => (
+                                        <option key={index} value={guide?.email}>
+                                            {guide?.name}
+                                        </option>
+                                    ))}
+                                </select>
+                            </div>
+                            <div className="col-span-full sm:col-span-3">
+                                <label htmlFor="price" className="text-sm">Price</label>
+                                <input id="price" type="text" defaultValue={`$${tour?.price}`} readOnly placeholder="$" className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" spellCheck="false" data-ms-editor="true" />
+                            </div>
+                            <div className="col-span-full">
+                                <label htmlFor="date" className="text-sm">Tour Date</label>
+                                <textarea id="date" placeholder="" className="w-full rounded-md focus:ring focus:ri focus:ri dark:border-gray-700 dark:text-gray-900" spellCheck="false" data-ms-editor="true"></textarea>
+                            </div>
+
+                        </div>
+
+                    </fieldset>
+                    <div className="w-full flex mx-auto justify-between flex-row">
+                        <div className="w-1/2">
+                            <label htmlFor="bio" className="">Photo</label>
+                            <div className="flex items-center space-x-2">
+                                <img src={"https://source.unsplash.com/30x30/?random"} alt="" className="w-36 h-36 rounded-box dark:text-white dark:bg-gray-700" />
+                                <button type="button" className="px-4 py-2 border rounded-2xl dark:border-gray-100">Change</button>
+                            </div>
+                        </div>
+                        <div className="w-1/2 flex items-end">
+                            <button className="btn btn-primary w-full">Book Now</button>
+                        </div>
+                    </div>
+                </form>
+            </section>
+
         </div>
     );
 };
