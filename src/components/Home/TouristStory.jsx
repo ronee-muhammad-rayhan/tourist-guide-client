368ca8738a41a4e559a488297a14bcff290de5a3
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2023-11-30 23:34:56 +0600

    story section added

--- src/components/Home/TouristStory.jsx
+++ src/components/Home/TouristStory.jsx
@@ -1,8 +1,93 @@
-
+import { FacebookShareButton, FacebookIcon } from "react-share";
 const TouristStory = () => {
     return (
-        <div>
-            Tourist Story Section:
+        <div className="py-12">
+            <h3 className="text-4xl">Tourist Story Section:</h3>
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4">
+                <div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md dark:divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
+                    <div className="flex justify-between p-4">
+                        <div className="flex space-x-4">
+                            <div>
+                                <img src="https://source.unsplash.com/100x100/?portrait" alt="" className="object-cover w-12 h-12 rounded-full dark:bg-gray-500" />
+                            </div>
+                            <div>
+                                <h4 className="font-bold">Leroy Jenkins</h4>
+                                <span className="text-xs dark:text-gray-400">2 days ago</span>
+                            </div>
+                        </div>
+                        <div className="flex items-center space-x-2 dark:text-yellow-500">
+                            <FacebookShareButton url={`https://tour-guide-rmr.web.app`} ><FacebookIcon size={32} round={true} /></FacebookShareButton>
+                        </div>
+                    </div>
+                    <div className="p-4 space-y-2 text-sm dark:text-gray-400">
+                        <p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor id justo fringilla finibus.</p>
+                        <p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
+                    </div>
+                </div>
+
+                <div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md dark:divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
+                    <div className="flex justify-between p-4">
+                        <div className="flex space-x-4">
+                            <div>
+                                <img src="https://source.unsplash.com/100x100/?portrait" alt="" className="object-cover w-12 h-12 rounded-full dark:bg-gray-500" />
+                            </div>
+                            <div>
+                                <h4 className="font-bold">Leroy Jenkins</h4>
+                                <span className="text-xs dark:text-gray-400">2 days ago</span>
+                            </div>
+                        </div>
+                        <div className="flex items-center space-x-2 dark:text-yellow-500">
+                            <FacebookShareButton url={`https://tour-guide-rmr.web.app`} ><FacebookIcon size={32} round={true} /></FacebookShareButton>
+                        </div>
+                    </div>
+                    <div className="p-4 space-y-2 text-sm dark:text-gray-400">
+                        <p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor id justo fringilla finibus.</p>
+                        <p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
+                    </div>
+                </div>
+
+                <div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md dark:divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
+                    <div className="flex justify-between p-4">
+                        <div className="flex space-x-4">
+                            <div>
+                                <img src="https://source.unsplash.com/100x100/?portrait" alt="" className="object-cover w-12 h-12 rounded-full dark:bg-gray-500" />
+                            </div>
+                            <div>
+                                <h4 className="font-bold">Leroy Jenkins</h4>
+                                <span className="text-xs dark:text-gray-400">2 days ago</span>
+                            </div>
+                        </div>
+                        <div className="flex items-center space-x-2 dark:text-yellow-500">
+                            <FacebookShareButton url={`https://tour-guide-rmr.web.app`} ><FacebookIcon size={32} round={true} /></FacebookShareButton>
+                        </div>
+                    </div>
+                    <div className="p-4 space-y-2 text-sm dark:text-gray-400">
+                        <p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor id justo fringilla finibus.</p>
+                        <p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
+                    </div>
+                </div>
+
+                <div className="container flex flex-col w-full max-w-lg p-6 mx-auto divide-y rounded-md dark:divide-gray-700 dark:bg-gray-900 dark:text-gray-100">
+                    <div className="flex justify-between p-4">
+                        <div className="flex space-x-4">
+                            <div>
+                                <img src="https://source.unsplash.com/100x100/?portrait" alt="" className="object-cover w-12 h-12 rounded-full dark:bg-gray-500" />
+                            </div>
+                            <div>
+                                <h4 className="font-bold">Leroy Jenkins</h4>
+                                <span className="text-xs dark:text-gray-400">2 days ago</span>
+                            </div>
+                        </div>
+                        <div className="flex items-center space-x-2 dark:text-yellow-500">
+                            <FacebookShareButton url={`https://tour-guide-rmr.web.app`} ><FacebookIcon size={32} round={true} /></FacebookShareButton>
+                        </div>
+                    </div>
+                    <div className="p-4 space-y-2 text-sm dark:text-gray-400">
+                        <p>Vivamus sit amet turpis leo. Praesent varius eleifend elit, eu dictum lectus consequat vitae. Etiam ut dolor id justo fringilla finibus.</p>
+                        <p>Donec eget ultricies diam, eu molestie arcu. Etiam nec lacus eu mauris cursus venenatis. Maecenas gravida urna vitae accumsan feugiat. Vestibulum commodo, ante sit urna purus rutrum sem.</p>
+                    </div>
+                </div>
+            </div>
         </div>
     );
 };
