adad0d165bea74dbf1320e6f4044dee7efe73446
RoneeMRayhan <roneemrayhan@rayhanworld.com>
2024-01-07 19:48:13 +0600

    cta and aos added

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,37 @@ import TourismTravelGuide from '../../components/Home/TourismTravelGuide';
 import { Helmet } from 'react-helmet-async';
 import TourType from '../../components/Home/TourType';
 import TouristStory from '../../components/Home/TouristStory';
+import CallToAction from '../../components/Home/CallToAction';
+import AOS from 'aos';
+import 'aos/dist/aos.css'; // You can also use <link> for styles
+// ..
+// AOS.init();
+// AOS.init();
+
+// You can also pass an optional settings object
+// below listed default settings
+AOS.init({
+    // Global settings:
+    disable: false, // accepts following values: 'phone', 'tablet', 'mobile', boolean, expression or function
+    startEvent: 'DOMContentLoaded', // name of the event dispatched on the document, that AOS should initialize on
+    initClassName: 'aos-init', // class applied after initialization
+    animatedClassName: 'aos-animate', // class applied on animation
+    useClassNames: false, // if true, will add content of `data-aos` as classes on scroll
+    disableMutationObserver: false, // disables automatic mutations' detections (advanced)
+    debounceDelay: 50, // the delay on debounce used while resizing window (advanced)
+    throttleDelay: 99, // the delay on throttle used while scrolling the page (advanced)
+
+
+    // Settings that can be overridden on per-element basis, by `data-aos-*` attributes:
+    offset: 120, // offset (in px) from the original trigger point
+    delay: 0, // values from 0 to 3000, with step 50ms
+    duration: 400, // values from 0 to 3000, with step 50ms
+    easing: 'ease', // default easing for AOS animations
+    once: false, // whether animation should happen only once - while scrolling down
+    mirror: false, // whether elements should animate out while scrolling past them
+    anchorPlacement: 'top-bottom', // defines which position of the element regarding to window should trigger the animation
+
+});
 
 const Home = () => {
     return (
@@ -16,7 +47,7 @@ const Home = () => {
                 <title>Tourists Guide | Home</title>
             </Helmet>
             {/* Banner/Slider Section */}
-            <section>
+            <section className='pb-12' data-aos="fade-up">
                 <Swiper id='swiper-banner'
                     spaceBetween={50}
                     slidesPerView={3}
@@ -32,18 +63,23 @@ const Home = () => {
             </section>
 
             {/* Tourism and Travel Guide Section */}
-            <section className='py-12'>
+            <section className='py-12' data-aos="zoom-in-right">
                 <TourismTravelGuide />
             </section>
 
             {/* Tour Type Section */}
-            <section>
+            <section className='py-12' data-aos="zoom-in-up">
                 <TourType />
             </section>
 
             {/* Tourist Story Section: */}
-            <section>
+            <section className='py-12' data-aos="flip-up">
                 <TouristStory />
+            </section>
+
+            {/* CallToAction Section: */}
+            <section className='py-12' data-aos="zoom-in-up">
+                <CallToAction />
             </section>
         </div>
     );
